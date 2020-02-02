350fb4a35a63c3ce01a2cdfe9ccee9767b696715
Rutul Shah <rutulshah2007@gmail.com>
2020-02-02 15:50:08 +0530

    Home page initial checkin

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -5,13 +5,25 @@ class Home extends Component {
         super();
         this.state = {
             postDetails: [],
-            postDetailsSnapshot: [],
-            commentTextField: [],
-            comments: [],
-            profileDetails: {},
-            accessToken: sessionStorage.getItem('access-token')
         }
     }
+
+    componentWillMount() {
+        let data = null;
+        let xhr = new XMLHttpRequest();
+        let that = this;
+        xhr.addEventListener("readystatechange", function () {
+            if(this.readyState === 4) {
+                that.setState({
+                    postDetails: JSON.parse(this.responseText).data
+                });
+                console.log(that.state.postDetails);
+            }
+        });
+        xhr.open("GET", "https://api.instagram.com/v1/users/self/media/recent?access_token=8661035776.d0fcd39.39f63ab2f88d4f9c92b0862729ee2784");
+        xhr.send(data);
+    }
+
     render() {
         return(
             <Header onSearchTextChanged={this.onSearchTextChangedHandler}></Header>

3331b0c6d9c24703985769fe4de8656adcedc6fc
Rutul Shah <rutulshah2007@gmail.com>
2020-02-02 16:42:37 +0530

    Added Home page Post structure

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,5 +1,44 @@
 import React, { Component } from 'react';
 import Header from '../../common/Header';
+import { CardContent, Avatar, GridList, Card, GridListTile, withStyles, InputLabel, FormControl, Input, Button } from '@material-ui/core';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper
+    },
+    upcomingMoviesHeading: {
+        textAlign: 'center',
+        background: '#ff9999',
+        padding: '8px',
+        fontSize: '1rem'
+    },
+    gridListUpcomingMovies: {
+        flexWrap: 'nowrap',
+        transform: 'translateZ(0)',
+        width: '100%'
+    },
+    gridListMain: {
+        transform: 'translateZ(0)',
+        cursor: 'pointer'
+    },
+    formControl: {
+        margin: theme.spacing.unit,
+        minWidth: 240,
+        maxWidth: 240
+    },
+    title: {
+        color: theme.palette.primary.light,
+    }
+});
+const cardStyle = {
+    // margin: 'auto',
+    width: '100%',
+    height: '100%',
+    padding: 50,
+}
+
 class Home extends Component {
     constructor() {
         super();
@@ -13,7 +52,7 @@ class Home extends Component {
         let xhr = new XMLHttpRequest();
         let that = this;
         xhr.addEventListener("readystatechange", function () {
-            if(this.readyState === 4) {
+            if (this.readyState === 4) {
                 that.setState({
                     postDetails: JSON.parse(this.responseText).data
                 });
@@ -25,10 +64,48 @@ class Home extends Component {
     }
 
     render() {
-        return(
-            <Header onSearchTextChanged={this.onSearchTextChangedHandler}></Header>
+        const { classes } = this.props;
+        return (
+            <div>
+                <Header onSearchTextChanged={this.onSearchTextChangedHandler}></Header>
+                <GridList cols={2} cellHeight={750} cols={2} className={classes.gridListMain}>
+                    {this.state.postDetails.map(p => (
+                        <GridListTile key={"title" + p.id} style={{ border: "1px solid black" }}>
+                            <Card style={{ cardStyle }} variant="outlined">
+                                <CardContent>
+                                    <Avatar src={p.user.profile_picture}></Avatar>
+                                    <span>
+                                        {p.user.username}
+                                        {p.created_time}
+                                    </span>
+
+                                    <img src={p.images.standard_resolution.url}></img>
+                                </CardContent>
+                                <div>
+                                    {p.caption.text}
+                                </div>
+                                <div>
+                                    {p.tags.map(tag => (
+                                        <span key={"tags" + tag}>#{tag}</span>
+                                    ))}
+                                </div>
+                                <div>
+                                    <span> <FavoriteBorderIcon />{p.likes.count} Likes</span>
+                                </div>
+                                <div>
+                                        <FormControl required style={{ width: "100%" }}>
+                                            <InputLabel htmlFor="username">Username</InputLabel>
+                                            <Input type="text" />
+                                        </FormControl>
+                                        <Button className="login-button" variant="contained" color="primary">Add</Button>
+                                    </div>
+                            </Card>
+                        </GridListTile>
+                    ))}
+                </GridList>
+            </div>
         );
     }
 }
 
-export default Home;
+export default withStyles(styles)(Home);
